ab65f9527deec9d792d5bc93a1549e4b66be3a4a
m-m-kh <khademim217@gmail.com>
2025-11-28 16:36:20 +0330

    create custom user model

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,13 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'silk',
-
     'rest_framework',
     'django_filters',
-    'corsheaders'
+    'corsheaders',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
+    'silk.middleware.SilkyMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 
     'corsheaders.middleware.CorsMiddleware',
 ]
@@ -123,13 +125,17 @@ USE_TZ = True
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 
+STATIC_URL = "/static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = [
-    "*"
-]
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# CORS_ALLOWED_ORIGINS = [
+#     "*"
+# ]
 # CORS_ALLOW_ALL_ORIGINS = True
